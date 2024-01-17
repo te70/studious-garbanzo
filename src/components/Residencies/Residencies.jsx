90b4c698cd9c3f5c280186a2ec04322db1658b5f
te70 <tevinkinuthia@gmail.com>
2024-01-17 13:17:49 +0300

    Add slider

--- src/components/Residencies/Residencies.jsx
+++ src/components/Residencies/Residencies.jsx
@@ -3,11 +3,46 @@ import {Swiper, SwiperSlide, useSwiper} from 'swiper/react'
 import "swiper/css"
 import "./Residencies.css"
 import data from "../../utils/slider.json"
+import { sliderSettings } from '../../utils/common'
 
 function Residencies() {
   return (
-    <div>Residencies</div>
+    <section className='r-wrapper'>
+      <div className='paddings innerWidth r-container'>
+        <div className="r-head flexColStart">
+          <span className='orangeText'>Best choices</span>
+          <span className='primaryText'>Popular Residencies</span>
+        </div>
+        <Swiper {...sliderSettings}>
+          <SliderButtons/>
+          {
+            data.map((card, i)=>(
+              <SwiperSlide key={i}>
+                <div className="r-card flexColStart">
+                  <img src={card.image} alt="home"/>
+                  <span className='secondaryText r-price'>
+                    <span style={{color: "orange"}}>$</span><span>{card.price}</span>
+                  </span>
+                  <span className='primaryText'>{card.name}</span>
+                  <span className='secondaryText'>{card.detail}</span>
+                </div>
+              </SwiperSlide>
+            ))
+          }
+        </Swiper>
+      </div>
+    </section>
   )
 }
 
-export default Residencies
+export default Residencies;
+
+const SliderButtons = ()=> {
+  const swiper = useSwiper();
+  return (
+    <div className='flexCenter r-button'>
+     <button onClick={()=>swiper.slidePrev()}>&lt;</button>
+     <button onClick={()=>swiper.slideNext()}>&gt;</button>
+    </div>
+  )
+}
