15e548471a5f7cc25efcee077bccc2e3f0164593
te70 <tevinkinuthia@gmail.com>
2024-01-17 12:42:06 +0300

    Add files

---
+++ src/components/Residencies/Residencies.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Swiper, SwiperSlide, useSwiper} from 'swiper/react'
+import "swiper/css"
+import "./Residencies.css"
+import data from "../../utils/slider.json"
+
+function Residencies() {
+  return (
+    <div>Residencies</div>
+  )
+}
+
+export default Residencies
